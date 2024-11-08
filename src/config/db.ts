c1457ff76c74899e6ee2e9548e058cec93a1c384
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-11-08 17:38:25 +0200

    doc

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,13 +2,13 @@ import {Sequelize} from 'sequelize'
 
 const sequelize = new Sequelize({
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: "",
-    database: "BloggingSystem",
+    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'BloggingSystem',
     logging: false,
     dialectOptions: {
-        charset: 'utf8mb4', // Set a valid character encoding
+        charset: 'utf8mb4', 
       },
 })
 
